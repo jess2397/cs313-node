416f17f1c9049f08c91a48f0eee0f239f1f1bc1b
jess2397 <eva15004@byui.edu>
2019-03-05 13:46:32 -0700

    assign 09 stuff

--- index.js
+++ index.js
@@ -7,4 +7,52 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/getRate', mailCalc)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+function mailCalc(req, res){
+
+    console.log("got here");
+
+    var type = req.query.type;
+    var weight = req.query.weight;
+
+    var typeName;
+    var result;
+    switch(type){
+        case 'S':
+            result = .55 + ((weight -1) * .15);
+            if(result > 1)
+                result = 1;
+            typeName = "Letters (Stamped)";
+            break;
+        case 'M':
+            result = .50 + ((weight -1) * .15);
+            if(result > .95)
+                result = 1;
+            typeName = "Letters (Metered)";
+            break;
+        case 'F':
+            result = 1.00 + ((weight - 1) *.15);
+            typeName = "Large Envelopes (Flats)";
+            break;
+        case 'R':
+            if(weight < 4)
+                result = 3.66;
+            else if(weight < 8)
+                result = 4.39;
+            else if (weight < 12)
+                result = 5.19;
+            else
+                result = 5.71;
+
+            typeName = "First-Class Package Service --Retail";
+            break;
+    }
+    console.log(result);
+
+    const params = {rate: result, weight: weight, type: typeName};
+
+    res.render("pages/getRate", params);
+}
